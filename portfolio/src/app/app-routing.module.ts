b6c584237a2d478c4f395ce3e69bc7b518104477
doumova <doumogueye.mg@gmail.com>
2022-05-12 01:52:30 +0000

    deploying my portfolio on heroku

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './components/body/body.component';
 import { ExperiencesComponent } from './components/experiences/experiences.component';
 import { FormationsComponent } from './components/formations/formations.component';
+import { HeaderComponent } from './components/header/header.component';
 import { WorkedonComponent } from './components/workedon/workedon.component';
 
 
